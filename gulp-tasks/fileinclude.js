ef651573afcc90e86637a1b4bc63dd903208d7f5
rhinodontypicus <rhinodontypicus@gmail.com>
2017-05-19 21:36:24 +0300

    allow multifolder structure

--- gulp-tasks/fileinclude.js
+++ gulp-tasks/fileinclude.js
@@ -3,10 +3,12 @@ const fileInclude = require('gulp-file-include');
 
 module.exports = function () {
     return this.gulp.src([
-        `${config.get('html.sourceDir')}/*.html`,
+        `${config.get('html.sourceDir')}/**/*.html`,
         `!${config.get('html.sourceDir')}/${config.get('html.includesDir')}/*.html`
-    ]).pipe(fileInclude({
+    ], {base: config.get('html.sourceDir')}).pipe(fileInclude({
         prefix: '@@',
         basepath: `${config.get('html.sourceDir')}/${config.get('html.includesDir')}`
-    })).pipe(this.gulp.dest(config.get('html.targetDir')));
+    })).pipe(this.gulp.dest(
+        config.get('html.targetDir')
+    ));
 };
